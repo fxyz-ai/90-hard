b9881d0dbc708cd66627a60f44a4bbaeae631a20
Mohamed Faiz <120184609+fxyz-ai@users.noreply.github.com>
2023-11-29 22:58:03 +0530

    day 64 commit

---
+++ 64. dashboard-design/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('menu');
+    const navigation = document.getElementById('navigation');
+    hamburger.addEventListener('click', function () {
+        navigation.classList.toggle('active')
+        console.log('clicked');
+    })
+})
