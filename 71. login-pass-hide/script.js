f019a19819e14b897834db8641d1ffcd71e15db1
Mohamed Faiz M <faizmd614@gmail.com>
2023-12-06 21:32:54 +0530

    day 71 commit

---
+++ 71. login-pass-hide/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const showHide = document.getElementById('show-hide');
+    let passwordInput = document.getElementById('password');
+
+    showHide.addEventListener('click', function () {
+        showHide.classList.toggle('show');
+
+        if (showHide.classList.contains('show')) {
+            showHide.classList.remove('fa-eye-slash');
+            showHide.classList.add('fa-eye');
+            passwordInput.setAttribute('type', 'text');
+        }
+        else {
+            showHide.classList.add('fa-eye-slash');
+            showHide.classList.remove('fa-eye');
+            passwordInput.setAttribute('type', 'password');
+        }
+    });
+});
