843dfbf3dc0d23d28aed17650dd72f7d4f788f10
Mohamed Faiz M <faizmd614@gmail.com>
2023-12-19 20:00:28 +0530

    day 84 commit

---
+++ 84. bg-slide/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var swiper = new Swiper(".swiper", {
+        grabCursor: true,
+        speed: 500,
+        effect: "slide",
+        loop: true,
+        mousewheel: {
+          invert: false,
+          sensitivity: 1,
+        },
+      });
+      
+      swiper.enable();
+})
